bdb9f5ec4f4fba87fd593726cfc9eb873de5eeff
torin6094 <jinchunji@gmail.com>
2026-05-06 10:05:32 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/weather-api': {
+        target: 'https://devapi.qweather.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/weather-api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            proxyReq.setHeader('Referer', 'https://lingzhiwu.com');
+            proxyReq.setHeader('Origin', 'https://lingzhiwu.com');
+          });
+        },
+      },
+    },
+  },
+});
