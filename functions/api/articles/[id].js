230cbbb3da07c70a484351f0a56c41102ffa953c
torin6094 <jinchunji@gmail.com>
2026-05-20 15:42:52 +0800

    fix: reduce cache to 60s for article detail API

--- functions/api/articles/[id].js
+++ functions/api/articles/[id].js
@@ -47,7 +47,7 @@ export async function onRequest(context) {
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
-        'Cache-Control': 'public, max-age=3600'
+        'Cache-Control': 'public, max-age=60'
       }
     })
   } catch (error) {
